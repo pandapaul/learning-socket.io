d46d3278abd71cd008479b40dd6ee7a35bc2b7ef
Paul Anderson <panderson@add123.com>
2016-09-27 12:43:28 -0400

    setting up some static html

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 'use strict';
-const app = require('express')();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
+app.use(express.static('static'));
 
 app.listen(3000, function(){
   console.log('listening on *:3000');
